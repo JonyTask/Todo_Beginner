4e52f74ab5e24a407bbd55b716fcd7c0b994eebf
JonyTask <yuusu.hyb@gmail.com>
2024-03-10 21:02:11 +0900

    fix : layout

--- next_frontend/src/components/Header.tsx
+++ next_frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     const header_title: string = "FullStack Todo --feat.Laravel10×Nextjs14"
 
     return(
-        <header className="bg-indigo-300 px-16 py-8">
+        <header className="bg-indigo-300 px-16 py-8 mb-20">
 			<h1 className="font-black italic font-mono antialiased text-2xl">{ header_title }</h1>
 		</header>
     )
